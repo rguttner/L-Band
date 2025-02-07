a00b9069a2ec580a6601aaf98c1513918b713626
Roman Guttner <rguttner@gmail.com>
2025-02-07 18:29:32 +0100

    update settings.php

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -266,7 +266,7 @@ $settings['config_sync_directory'] = 'config/sync';
  *   $settings['hash_salt'] = file_get_contents('/home/example/salt.txt');
  * @endcode
  */
-$settings['hash_salt'] = '';
+$settings['hash_salt'] = 'dsfkjsdfiW454354TDFGSDFGDS';
 
 /**
  * Deployment identifier.
